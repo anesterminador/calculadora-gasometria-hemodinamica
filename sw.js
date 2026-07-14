293c160c7164be0b14a9f820f878f39f155cecd5
anesterminador <anesterminador@users.noreply.github.com>
2026-07-14 00:47:34 -0300

    Atualizacao do site

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hemodinamica-v104';
+const CACHE_NAME = 'hemodinamica-v105';
 
 const ASSETS = [
   '/',
@@ -122,6 +122,7 @@ self.addEventListener('fetch', event => {
       .catch(() => caches.match(request).then(cached => cached || fetch(request)))
   );
 });
+
 
 
 
